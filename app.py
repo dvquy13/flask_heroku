c2edf87597672f1f53506ac27daa18954069184b
C4E3 CMS <quy.dinh3195@gmail.com>
2016-04-12 20:42:51 +0700

    Deploy CMS Flask App

--- app.py
+++ app.py
@@ -1,62 +1,81 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+from mongoengine import *
+from flask import *
 
-This file creates your application.
-"""
 
-import os
-from flask import Flask, render_template, request, redirect, url_for
+#Upload data to Mongo
+connect('nim', host='mongodb://hiep:hiepxanh@ds015929.mlab.com:15929/nim')
+
+class Lecture(Document):
+    href = StringField()
+    title = StringField()
+    brief = ListField(StringField())
+    document = ListField(DictField())
+    assignment = ListField(DictField())
+
+
+
+class Users(Document):
+    username = StringField()
+    password = StringField()
+
+# user1 = Users(username = "dvquy",
+#               password = "dvquy123")
+# user2 = Users(username = "ntnhan",
+#               password = "ntnhan123")
+# user2.save()
+# user1.save()
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    user_list = Users.objects
+    error = ""
+    try:
+        if request.method == "POST":
+            u = request.form['username']
+            p = request.form['password']
+            for user in user_list:
+                if user.username == u and user.password == p:
+                    # session["logged_in"] = True
+                    # session["username"] = request.form("username")
+                    return redirect(url_for("index"))
+                    # return render_template("Index.html",
+                                           # username = user.username,
+                    #                        lecture_list = lecture_list)
+            error = "Invalid credentials. Please try again!"
 
+    except Exception as e:
+        flash(e)
+        return render_template("login.html", error = error)
 
-###
-# Routing for your application.
-###
+    return render_template('login.html', error = error)
 
-@app.route('/')
-def home():
-    """Render website's home page."""
-    # return render_template('home.html')
-    return "casdlfja;lsdfja;lsdf"
+@app.route('/<lectureHref>/')
+def lectureHref(lectureHref):
+    lecture_list = Lecture.objects
+    for lecture in lecture_list:
+        if lecture.href == lectureHref:
+            return render_template("lecture.html",
+                                   lecture = lecture,
+                                   lecture_list = lecture_list)
+#Lay anh tai local host:
+@app.route('/<path:path>/')
+def static_file(path):
+    return app.send_static_file(path)
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+@app.route('/index/')
+def index():
+    lecture_list = Lecture.objects
+    return render_template("Index.html",
+                           lecture_list = lecture_list)
 
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
+def page_not_found(e):
+    return render_template("404.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
